f3ef64980938e9b619fdb138ddd5a306964fb45c
Norovl <novikovrv1@mail.ru>
2020-04-05 16:49:30 +0600

    Update button.ino

--- firmware/GyverLamp_v1.4_Arduino_v1.2/button.ino
+++ firmware/GyverLamp_v1.4_Arduino_v1.2/button.ino
@@ -4,42 +4,10 @@ boolean brightDirection, speedDirection, scaleDirection;
 void buttonTick() {
   touch.tick();
 
-  if (touch.isSingle()) {
-    //    if (dawnFlag) {
-    //      //manualOff = true;
-    //      dawnFlag = false;
-    //      loadingFlag = true;
-    //      FastLED.setBrightness(modes[currentMode].brightness);
-    //      changePower();
-    //    } else
-    {
-      if (ONflag) {
-        ONflag = false;
-        changePower();
-      } else {
-        ONflag = true;
-        changePower();
-      }
-    }
-  }
+
 
   if (ONflag) {                 // если включено
-    if (touch.isDouble()) {
-      if (++currentMode >= MODE_AMOUNT) currentMode = 0;
-      FastLED.setBrightness(modes[currentMode].brightness);
-      loadingFlag = true;
-      //settChanged = true;
-      FastLED.clear();
-      delay(1);
-    }
-    if (touch.isTriple()) {
-      if (--currentMode < 0) currentMode = MODE_AMOUNT - 1;
-      FastLED.setBrightness(modes[currentMode].brightness);
-      loadingFlag = true;
-      //settChanged = true;
-      FastLED.clear();
-      delay(1);
-    }
+
 
     if ((touch.hasClicks()) && (touch.getClicks() == 5)) {      // если было пятикратное нажатие на кнопку, то производим сохранение параметров // && (touch.hasClicks())
       if (EEPROM.read(0) != 102) EEPROM.write(0, 102);
@@ -56,48 +24,56 @@ void buttonTick() {
       ONflag = true;
       changePower();
     }
+    //
+    //    if (touch.isStep()) {  // изменение яркости при удержании кнопки
+    //      if (numHold != 1) brightDirection = !brightDirection;
+    //      numHold = 1;
+    //    }
+    //
+    //    if (touch.isStep(1)) {  // изменение скорости "speed" при двойном нажатии и удержании кнопки
+    //      if (numHold != 2) speedDirection = !speedDirection;
+    //      numHold = 2;
+    //    }
+    //
+    //    if (touch.isStep(2)) {  // изменение масштаба "scale" при тройном нажатии и удержании кнопки
+    //      if (numHold != 3) scaleDirection = !scaleDirection;
+    //      numHold = 3;
+    //    }
 
-    if (touch.isStep()) {  // изменение яркости при удержании кнопки
-      if (numHold != 1) brightDirection = !brightDirection;
-      numHold = 1;
-    }
-
-    if (touch.isStep(1)) {  // изменение скорости "speed" при двойном нажатии и удержании кнопки
-      if (numHold != 2) speedDirection = !speedDirection;
-      numHold = 2;
-    }
-
-    if (touch.isStep(2)) {  // изменение масштаба "scale" при тройном нажатии и удержании кнопки
-      if (numHold != 3) scaleDirection = !scaleDirection;
-      numHold = 3;
-    }
-
-    if (touch.isHold()) {
+    if (touch.isHold() && touch.isStep()) {
+      //numHold = touch.getHoldClicks();
       //  if (numHold != 0) {
-      //      Serial.print(numHold);
-      //      Serial.print(brightDirection);
-      //      Serial.print(speedDirection);
-      //      Serial.println(scaleDirection);
+      Serial.print(numHold);
+      Serial.print(brightDirection);
+      Serial.print(speedDirection);
+      Serial.println(scaleDirection);
       //    Serial.print(" brightness:");
       //    Serial.print(modes[currentMode].brightness);
       //    Serial.print(" speed:");
       //    Serial.print(modes[currentMode].speed);
       //    Serial.print(" scale:");
       //    Serial.println(modes[currentMode].scale);
-      if (numHold != 0) numHold_Timer = millis(); loadingFlag = true;
-      switch (numHold) {
+      // switch (numHold) {
+      switch (touch.getHoldClicks() + 1) {
         case 1:
+          if (numHold != 1) brightDirection = !brightDirection;
           modes[currentMode].brightness = constrain(modes[currentMode].brightness + (modes[currentMode].brightness / 25 + 1) * (brightDirection * 2 - 1), 1 , 255);
+          numHold = 1;
           break;
 
         case 2:
+          if (numHold != 2) speedDirection = !speedDirection;
           modes[currentMode].speed = constrain(modes[currentMode].speed + (modes[currentMode].speed / 25 + 1) * (speedDirection * 2 - 1), 1 , 255);
+          numHold = 2;
           break;
 
         case 3:
+          if (numHold != 3) scaleDirection = !scaleDirection;
           modes[currentMode].scale = constrain(modes[currentMode].scale + (modes[currentMode].scale / 25 + 1) * (scaleDirection * 2 - 1), 1 , 255);
+          numHold = 3;
           break;
       }
+      if (numHold != 0) numHold_Timer = millis(); loadingFlag = true;
     }
     if ((millis() - numHold_Timer) > numHold_Time) {
       numHold = 0;
@@ -105,5 +81,30 @@ void buttonTick() {
     }
     FastLED.setBrightness(modes[currentMode].brightness);
     //settChanged = true;
+    if (touch.isDouble()) {
+      if (++currentMode >= MODE_AMOUNT) currentMode = 0;
+      FastLED.setBrightness(modes[currentMode].brightness);
+      loadingFlag = true;
+      //settChanged = true;
+      FastLED.clear();
+      delay(1);
+    }
+    if (touch.isTriple()) {
+      if (--currentMode < 0) currentMode = MODE_AMOUNT - 1;
+      FastLED.setBrightness(modes[currentMode].brightness);
+      loadingFlag = true;
+      //settChanged = true;
+      FastLED.clear();
+      delay(1);
+    }
+  }
+  if (touch.isSingle()) {
+    if (ONflag) {
+      ONflag = false;
+      changePower();
+    } else {
+      ONflag = true;
+      changePower();
+    }
   }
 }
